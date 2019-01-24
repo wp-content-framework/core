8faad78f17141ed8700cb543e8224b12d9396e29
technote <technote.space@gmail.com>
2019-01-24 21:47:28 +0900

    v0.0.2

--- src/framework.php
+++ src/framework.php
@@ -2,9 +2,10 @@
 /**
  * WP_Framework
  *
- * @version 0.0.1
+ * @version 0.0.2
  * @author technote-space
  * @since 0.0.1
+ * @since 0.0.2 Added: send_mail method
  * @copyright technote-space All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space
