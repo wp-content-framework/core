2401da25d568e0b99503da1ffb6495801e1189cf
technote <technote.space@gmail.com>
2019-02-03 03:24:28 +0900

    v0.0.24

--- src/traits/singleton.php
+++ src/traits/singleton.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Core Traits Singleton
  *
- * @version 0.0.23
+ * @version 0.0.24
  * @author technote-space
  * @copyright technote-space All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
