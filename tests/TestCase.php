6d8334b11dd58eaa6e74424f74c1cec355e69b12
technote <technote.space@gmail.com>
2019-02-08 02:21:58 +0900

    #30

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -32,6 +32,11 @@ class TestCase extends \PHPUnit\Framework\TestCase {
 	 */
 	protected static $plugin_file;
 
+	/**
+	 * @var string
+	 */
+	protected static $plugin_dir;
+
 	public static function setUpBeforeClass() {
 		static::$app = \Phake::mock( '\WP_Framework' );
 		\Phake::when( static::$app )->get_package_directory()->thenReturn( dirname( dirname( __FILE__ ) ) );
@@ -39,10 +44,12 @@ class TestCase extends \PHPUnit\Framework\TestCase {
 		\Phake::when( static::$app )->has_initialized()->thenReturn( true );
 		\Phake::when( static::$app )->is_enough_version()->thenReturn( true );
 		\Phake::when( static::$app )->get_packages()->thenReturn( [] );
-		static::$plugin_name      = md5( uniqid() );
-		static::$plugin_file      = __FILE__;
+		static::$plugin_name = md5( uniqid() );
+		static::$plugin_file = __FILE__;
+		static::$plugin_dir  = dirname( __FILE__ );
 		\Phake::when( static::$app )->__get( 'plugin_name' )->thenReturn( static::$plugin_name );
 		\Phake::when( static::$app )->__get( 'plugin_file' )->thenReturn( static::$plugin_file );
+		\Phake::when( static::$app )->__get( 'plugin_dir' )->thenReturn( static::$plugin_dir );
 		\Phake::when( static::$app )->__get( 'slug_name' )->thenReturn( static::$plugin_name );
 		\Phake::when( static::$app )->__get( 'define' )->thenReturn( \WP_Framework_Common\Classes\Models\Define::get_instance( static::$app ) );
 		\Phake::when( static::$app )->__get( 'input' )->thenReturn( \WP_Framework_Common\Classes\Models\Input::get_instance( static::$app ) );
