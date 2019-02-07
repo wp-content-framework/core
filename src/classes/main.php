e9015c8395ffdc921d73cf0e89cea29cced4fc93
technote <technote.space@gmail.com>
2019-02-08 00:38:49 +0900

    v0.0.29

--- src/classes/main.php
+++ src/classes/main.php
@@ -242,6 +242,10 @@ class Main {
 		$this->filter->do_action( 'app_initialize', $this );
 		$this->setup_property();
 		$this->filter->do_action( 'app_initialized', $this );
+
+		if ( ! $this->option->is_app_activated() ) {
+			$this->filter->do_action( 'app_activated' );
+		}
 	}
 
 	/**
