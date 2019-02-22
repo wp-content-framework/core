7be86f54392b81762f0acfa1341cc9e5f18b5cee
technote <technote.space@gmail.com>
2019-02-23 01:57:58 +0900

    v0.0.37

--- src/traits/hook.php
+++ src/traits/hook.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Core Traits Hook
  *
- * @version 0.0.21
+ * @version 0.0.37
  * @author technote-space
  * @copyright technote-space All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
@@ -22,7 +22,10 @@ if ( ! defined( 'WP_CONTENT_FRAMEWORK' ) ) {
  */
 trait Hook {
 
-	private $_filter_prefix;
+	/**
+	 * @var string $_filter_prefix
+	 */
+	private $_filter_prefix = null;
 
 	/**
 	 * load cache settings
