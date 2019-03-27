34a2e9f7e6d02bbe2f80a9edd7644602fca37255
Technote <technote.space@gmail.com>
2019-03-27 21:00:41 +0900

    v0.0.50

--- src/framework.php
+++ src/framework.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework
  *
- * @version 0.0.49
+ * @version 0.0.50
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
