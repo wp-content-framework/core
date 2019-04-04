4e39bacb216b6c74d302347f541bab9cd9709c22
Technote <technote.space@gmail.com>
2019-04-04 13:21:15 +0900

    v0.0.52

--- src/traits/loader.php
+++ src/traits/loader.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Core Traits Loader
  *
- * @version 0.0.49
+ * @version 0.0.52
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
